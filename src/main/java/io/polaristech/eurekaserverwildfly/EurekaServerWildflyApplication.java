67a01481ee7851f742352adbd0fe0006092aae3e
caishu <caishu@polaristech.io>
2018-11-27 22:00:25 +0800

    add empty eureka server from start.spring.io

---
+++ src/main/java/io/polaristech/eurekaserverwildfly/EurekaServerWildflyApplication.java
@@ -0,0 +1,12 @@
+package io.polaristech.eurekaserverwildfly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerWildflyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerWildflyApplication.class, args);
+	}
+}
